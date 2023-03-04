cc2604fed73e6f6d0b09da5e8e317f2a95029e49
chouette11 <keifukuda1115@gmail.com>
2023-03-05 00:27:41 +0900

    Add header

--- src/compornent/Header.tsx
+++ src/compornent/Header.tsx
@@ -1,63 +1,75 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import React from "react";
+import { styled } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-  children: React.ReactElement;
-}
+const HeaderBar = styled(AppBar)({
+  background: "#673ab7",
+  paddingTop: "1rem",
+  paddingBottom: "1rem",
+  height: "80px",
+});
 
-function ElevationScroll(props: Props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
-  });
+const Title = styled(Box)({
+  flexGrow: 1,
+  textAlign: "center",
+  "& h1": {
+    fontSize: "2.5rem",
+    fontWeight: "bold",
+    margin: 0,
+    lineHeight: "1.2",
+    color: "#fff",
+    fontFamily: "Montserrat, sans-serif",
+  },
+});
 
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
-}
+const NavButton = styled(Button)({
+  color: "#fff",
+  "&:hover": {
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+  },
+});
 
-export default function ElevateAppBar(props: Props) {
+const NavBox = styled(Box)({
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center",
+});
+
+const Header = () => {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const id = event.currentTarget.getAttribute("href")!.substring(1);
+    const element = document.getElementById(id);
+    const yOffset = -80;
+    const y = element!.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: "smooth" });
+  };
+
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <ElevationScroll {...props}>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6" component="div">
-              Scroll to elevate App bar
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
-      <Toolbar />
-      <Container>
-        <Box sx={{ my: 2 }}>
-          {[...new Array(12)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-        </Box>
-      </Container>
-    </React.Fragment>
+    <HeaderBar position="fixed">
+      <Toolbar>
+        <Title>
+          <h1>App Name</h1>
+        </Title>
+        <NavBox>
+          <NavButton href="#feature1" onClick={handleClick}>
+            Feature 1
+          </NavButton>
+          <NavButton href="#feature2" onClick={handleClick}>
+            Feature 2
+          </NavButton>
+          <NavButton href="#feature3" onClick={handleClick}>
+            Feature 3
+          </NavButton>
+          <NavButton href="#feature4" onClick={handleClick}>
+            Feature 4
+          </NavButton>
+        </NavBox>
+      </Toolbar>
+    </HeaderBar>
   );
-}
+};
+
+export default Header;
