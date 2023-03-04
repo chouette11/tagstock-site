5c13490313ecc1cb0ef0e568b8fc082da05fb2b0
chouette11 <keifukuda1115@gmail.com>
2023-03-05 00:28:11 +0900

    Add screen shot

---
+++ src/compornent/AppScreenShot.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Typography, Box, Grid, Paper } from "@mui/material";
+
+interface AppScreenshotProps {
+  title: string;
+  image: string;
+}
+
+const AppScreenshot: React.FC<AppScreenshotProps> = ({ title, image }) => {
+  return (
+    <Grid container spacing={8} >
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h4" sx={{ fontWeight: "bold", mb: 2 }}>
+          My Awesome App
+        </Typography>
+        <Typography variant="subtitle1" sx={{ mb: 2 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor
+          faucibus justo, ac volutpat sapien dictum non. Sed nec justo eu nibh
+          dignissim aliquet.
+        </Typography>
+    
+      </Grid>
+      <Grid item xs={12} md={6} justifyContent="end">
+        <img src={image} alt={title} style={{ maxWidth: "100%" }} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AppScreenshot;
