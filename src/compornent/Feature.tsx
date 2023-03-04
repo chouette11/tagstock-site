c4689d3b79039d012c425996e10c15915955439f
chouette11 <keifukuda1115@gmail.com>
2023-03-05 00:27:58 +0900

    Add features

---
+++ src/compornent/Feature.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+
+type Props = {
+  title: string;
+  description: string;
+  imageUrl: string;
+};
+
+const Feature: React.FC<Props> = ({ title, description, imageUrl }) => {
+  return (
+    <div style={{
+        padding: "1rem",
+      }}>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" height="140" image={imageUrl} alt={title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+  );
+};
+
+export default Feature;
---
+++ src/compornent/Features.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import Feature from "./Feature";
+
+type Props = {
+  id: string;
+};
+
+const Features: React.FC<Props> = ({id}) => {
+  return (
+    <div id={id}>
+    <Grid direction="column">
+      <Typography variant="h4" align="center" sx={{ fontWeight: "bold", mb: 2 }}>
+        Features
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4}>
+          <Feature
+            title="Feature 1"
+            description="Description of Feature 1"
+            imageUrl="https://via.placeholder.com/150"
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Feature
+            title="Feature 2"
+            description="Description of Feature 2"
+            imageUrl="https://via.placeholder.com/150"
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Feature
+            title="Feature 3"
+            description="Description of Feature 3"
+            imageUrl="https://via.placeholder.com/150"
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+    </div>
+  );
+};
+
+export default Features;
